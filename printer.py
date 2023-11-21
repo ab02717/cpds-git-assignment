5bcbed08acb1a3f2fda82970a420ea7bd00f7d93
ab02717 <leon.mtshweni@absa.africa>
2023-11-21 14:01:04 +0200

    add feature print reversed

--- printer.py
+++ printer.py
@@ -1,3 +1,10 @@
+def print_reversed(s):
+    reversed = []
+    for item in range(len(s)-1,-1,-1):
+        reversed.append(s[item])
+    return ''.join(reversed)
+
+
 if __name__ == "__main__":
     s = input()
     print_reversed(s)
